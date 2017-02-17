0de70b37157d5fef523dac274c2b47e2925fa4d6
Carlos Anaya <jose.carlos.anaya@gmail.com>
2017-02-17 12:49:45 -0600

    Add functionality to cancel a gig

---
+++ GigHub/Controllers/api/GigsController.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Web.Http;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+
+namespace GigHub.Controllers.api
+{
+    [Authorize]
+    public class GigsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Cancel(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var gig = _context.Gigs.Single(g => g.Id == id && g.ArtistId == userId);
+
+            gig.IsCanceled = true;
+
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
