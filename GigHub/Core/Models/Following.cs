6683970e8e72298a77130978ff57bd1398abef3b
Carlos Anaya <jose.carlos.anaya@gmail.com>
2017-03-10 16:05:00 -0600

    Changing missing Data Annotations to Fluent API

--- GigHub/Core/Models/Following.cs
+++ GigHub/Core/Models/Following.cs
@@ -1,19 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace GigHub.Core.Models
+﻿namespace GigHub.Core.Models
 {
-    // Alternatively, this class could be called Relationship.
     public class Following
     {
-        [Key]
-        [Column(Order = 1)]
         public string FollowerId { get; set; }
-
-        [Key]
-        [Column(Order = 2)]
         public string FolloweeId { get; set; }
-
         public ApplicationUser Follower { get; set; }
         public ApplicationUser Followee { get; set; }
     }
--- GigHub/Core/Models/Gig.cs
+++ GigHub/Core/Models/Gig.cs
@@ -8,21 +8,13 @@ namespace GigHub.Core.Models
     public class Gig
     {
         public int Id { get; set; }
-
         public bool IsCanceled { get; private set; }
-
         public ApplicationUser Artist { get; set; }
-
         public string ArtistId { get; set; }
-
         public DateTime DateTime { get; set; }
-
         public string Venue { get; set; }
-
         public Genre Genre { get; set; }
-
         public byte GenreId { get; set; }
-
         public ICollection<Attendance> Attendances { get; }
 
         public Gig()
