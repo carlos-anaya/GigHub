9ab4e94bd7d43dac9cdde7be5b32da2854916e94
Carlos Anaya <jose.carlos.anaya@gmail.com>
2017-03-10 15:31:14 -0600

    Refactoring Data Annotations to Fluent API

---
+++ GigHub/Persistence/EntityConfigurations/GigConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using GigHub.Core.Models;
+using System.Data.Entity.ModelConfiguration;
+
+namespace GigHub.Persistence.EntityConfigurations
+{
+    public class GigConfiguration : EntityTypeConfiguration<Gig>
+    {
+        public GigConfiguration()
+        {
+            //First Key configurations
+
+            //Then Property configurations
+            Property(g => g.ArtistId)
+                .IsRequired();
+
+            Property(g => g.GenreId)
+                .IsRequired();
+
+            Property(g => g.Venue)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            //Finally, Relation configurations (always from parent)
+            HasMany(g => g.Attendances)
+                .WithRequired(g => g.Gig)
+                .WillCascadeOnDelete(false);
+        }
+    }
+}
